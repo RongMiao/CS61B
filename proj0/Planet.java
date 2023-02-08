92fd0a8942124b790825906527fdbe115a997ed5
rongmiao_wang <rongm325@qq.com>
2023-02-08 20:10:28 +0800

    add Planet

---
+++ proj0/Planet.java
@@ -0,0 +1,68 @@
+
+
+public class Planet {
+    
+    public double xxPos;
+    public double yyPos;
+    public double xxVel;
+    public double yyVel;
+    public double mass;
+    public String imgFileName;
+    
+    public Planet(double xP, double yP, double xV, double yV, double m, String img) {
+        xxPos = xP;
+        yyPos = yP;
+        xxVel = xV;
+        yyVel = yV;
+        mass = m;
+        imgFileName = img;
+    }
+    
+    public Planet(Planet p) {
+        this.xxPos = p.xxPos;
+        this.yyPos = p.yyPos;
+        this.xxVel = p.xxVel;
+        this.yyVel = p.yyVel;
+        this.mass = p.mass;
+        this.imgFileName = p.imgFileName;
+    }
+
+    public double calcDistance(Planet p) {
+        return Math.sqrt((xxPos-p.xxPos)*(xxPos-p.xxPos) + (yyPos-p.yyPos)*(yyPos-p.yyPos));
+    }
+
+    public double calcForceExertedByY(Planet p) {
+        return 6.67e-11*mass*p.mass/(calcDistance(p)*calcDistance(p))*(p.yyPos-yyPos)/calcDistance(p);
+    }
+
+    public double calcForceExertedByX(Planet p) {
+        return 6.67e-11*mass*p.mass/(calcDistance(p)*calcDistance(p))*(p.xxPos-xxPos)/calcDistance(p);
+    }
+
+    public double calcNetForceExertedByY(Planet[] p) {
+        double ret = 0;
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] == this)
+                continue;
+            ret += calcForceExertedByY(p[i]);
+        }
+        return ret;
+    }
+
+    public double calcNetForceExertedByX(Planet[] p) {
+        double ret = 0;
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] == this)
+                continue;
+            ret += calcForceExertedByX(p[i]);
+        }
+        return ret;
+    }
+
+    public void update(double dt, double xV, double yV) {
+        xxPos += (xxVel*dt + xV/mass*dt*dt);
+        yyPos += (yyVel*dt + yV/mass*dt*dt);
+        xxVel += dt*xV/mass;
+        yyVel += dt*yV/mass;
+    }
+}

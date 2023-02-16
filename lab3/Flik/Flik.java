b064468238de1b04cd9e40b82f9cc1344df331e2
rongmiao_wang <rongm325@qq.com>
2023-02-16 23:11:03 +0800

    finish lab3

--- lab3/Flik/Flik.java
+++ lab3/Flik/Flik.java
@@ -1,6 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /** An Integer tester created by Flik Enterprises. */
 public class Flik {
     public static boolean isSameNumber(Integer a, Integer b) {
-        return a == b;
+        return a.equals(b);
+    }
+
+    @Test
+    public void testIsSameNumber() {
+        assertTrue(isSameNumber(1,1));
+        assertTrue(isSameNumber(100,100));
+        assertTrue(isSameNumber(128, 128));
+        assertTrue(isSameNumber(200, 200));
     }
 }

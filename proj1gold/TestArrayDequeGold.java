378091de08795884fff3ad14d6ce58dbdc4a9229
rongmiao_wang <rongm325@qq.com>
2023-02-19 22:41:27 +0800

    finish proj1 gold 3

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -7,23 +7,23 @@ public class TestArrayDequeGold {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
         StringBuilder stringBuilder = new StringBuilder();
+
         for (int i = 0; i < 10; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.5) {
                 sad1.addLast(i);
                 sad2.addLast(i);
-                stringBuilder.append("addLast " + i + "\n");
+                stringBuilder.append(i + " ");
             } else {
                 sad1.addFirst(i);
                 sad2.addFirst(i);
-                stringBuilder.append("addFirst " + i + "\n");
+                stringBuilder.append(i + " ");
             }
         }
 
-        stringBuilder.append("size \n");
         assertEquals(stringBuilder.toString(), sad2.size(), sad1.size());
         for (int i = 0; i < sad1.size(); i++) {
-            assertEquals("index = " + i, sad2.get(i), sad1.get(i));
+            assertEquals(stringBuilder.toString(), sad2.get(i), sad1.get(i));
         }
 
         for (int i = 0; i < sad1.size(); i++) {
@@ -31,13 +31,11 @@ public class TestArrayDequeGold {
             if (numberBetweenZeroAndOne < 0.5) {
                 Integer item1 = sad1.removeLast();
                 Integer item2 = sad2.removeLast();
-                stringBuilder.append("removeLast\n");
-                assertNotNull(item1);
+                assertNotNull(stringBuilder.toString(), item1);
                 assertEquals(stringBuilder.toString(), item2, item1);
             } else {
                 Integer item1 = sad1.removeFirst();
                 Integer item2 = sad2.removeFirst();
-                stringBuilder.append("removeFirst\n");
                 assertEquals(stringBuilder.toString(), item2, item1);
             }
         }

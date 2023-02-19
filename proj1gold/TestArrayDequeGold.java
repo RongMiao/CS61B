501e21f2dd096d7ace198d0db954c8b2cba3bdc7
rongmiao_wang <rongm325@qq.com>
2023-02-19 22:59:29 +0800

    finish proj1 gold 4

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,24 +6,31 @@ public class TestArrayDequeGold {
     public void testGold() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
-        StringBuilder stringBuilder = new StringBuilder();
-
+        String sequence = new String();
         for (int i = 0; i < 10; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.5) {
                 sad1.addLast(i);
                 sad2.addLast(i);
-                stringBuilder.append(i + " ");
+                if (sequence.length() == 0) {
+                    sequence += i;
+                } else {
+                    sequence = i + " " + sequence;
+                }
             } else {
                 sad1.addFirst(i);
                 sad2.addFirst(i);
-                stringBuilder.append(i + " ");
+                if (sequence.length() == 0) {
+                    sequence += i;
+                } else {
+                    sequence = sequence + " " + i;
+                }
             }
         }
 
-        assertEquals(stringBuilder.toString(), sad2.size(), sad1.size());
+        assertEquals(sequence, sad2.size(), sad1.size());
         for (int i = 0; i < sad1.size(); i++) {
-            assertEquals(stringBuilder.toString(), sad2.get(i), sad1.get(i));
+            assertEquals(sequence, sad2.get(i), sad1.get(i));
         }
 
         for (int i = 0; i < sad1.size(); i++) {
@@ -31,12 +38,13 @@ public class TestArrayDequeGold {
             if (numberBetweenZeroAndOne < 0.5) {
                 Integer item1 = sad1.removeLast();
                 Integer item2 = sad2.removeLast();
-                assertNotNull(stringBuilder.toString(), item1);
-                assertEquals(stringBuilder.toString(), item2, item1);
+                assertNotNull(sequence, item1);
+                assertEquals(sequence, item2, item1);
             } else {
                 Integer item1 = sad1.removeFirst();
                 Integer item2 = sad2.removeFirst();
-                assertEquals(stringBuilder.toString(), item2, item1);
+                assertNotNull(sequence, item1);
+                assertEquals(sequence, item2, item1);
             }
         }
     }

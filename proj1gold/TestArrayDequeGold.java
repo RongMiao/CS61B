cff05bad0469c371d9dec821ce26a57ff4ebbeb5
rongmiao_wang <rongm325@qq.com>
2023-02-19 22:37:32 +0800

    finish proj1 gold 2

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -6,22 +6,24 @@ public class TestArrayDequeGold {
     public void testGold() {
         StudentArrayDeque<Integer> sad1 = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> sad2 = new ArrayDequeSolution<>();
-
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < 10; i++) {
             double numberBetweenZeroAndOne = StdRandom.uniform();
             if (numberBetweenZeroAndOne < 0.5) {
                 sad1.addLast(i);
                 sad2.addLast(i);
+                stringBuilder.append("addLast " + i + "\n");
             } else {
                 sad1.addFirst(i);
                 sad2.addFirst(i);
+                stringBuilder.append("addFirst " + i + "\n");
             }
         }
 
-        assertEquals("size is not euqal", sad2.size(), sad1.size());
+        stringBuilder.append("size \n");
+        assertEquals(stringBuilder.toString(), sad2.size(), sad1.size());
         for (int i = 0; i < sad1.size(); i++) {
-            assertEquals("index = " + i + " sda1 = " + sad1.get(i) + " sda2 = " + sad2.get(i),
-                    sad2.get(i), sad1.get(i));
+            assertEquals("index = " + i, sad2.get(i), sad1.get(i));
         }
 
         for (int i = 0; i < sad1.size(); i++) {
@@ -29,13 +31,15 @@ public class TestArrayDequeGold {
             if (numberBetweenZeroAndOne < 0.5) {
                 Integer item1 = sad1.removeLast();
                 Integer item2 = sad2.removeLast();
-                assertEquals("removeLast error", item2, item1);
+                stringBuilder.append("removeLast\n");
+                assertNotNull(item1);
+                assertEquals(stringBuilder.toString(), item2, item1);
             } else {
                 Integer item1 = sad1.removeFirst();
                 Integer item2 = sad2.removeFirst();
-                assertEquals("removeFirst error", item2, item1);
+                stringBuilder.append("removeFirst\n");
+                assertEquals(stringBuilder.toString(), item2, item1);
             }
-            assertEquals("size is not euqal after remove", sad2.size(), sad1.size());
         }
     }
 }

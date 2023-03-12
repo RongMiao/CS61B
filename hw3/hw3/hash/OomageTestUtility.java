1c1813facd002d21663b9552532aa5e10bc17620
rongmiao_wang <rongm325@qq.com>
2023-03-12 17:47:26 +0800

    finish hw 3

--- hw3/hw3/hash/OomageTestUtility.java
+++ hw3/hw3/hash/OomageTestUtility.java
@@ -1,5 +1,10 @@
 package hw3.hash;
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 
 public class OomageTestUtility {
@@ -12,6 +17,20 @@ public class OomageTestUtility {
          * and ensure that no bucket has fewer than N / 50
          * Oomages and no bucket has more than N / 2.5 Oomages.
          */
-        return false;
+
+        int size = oomages.size();
+        int[] arr = new int[M];
+        int low = size / 50;
+        int up = (int)(size / 2.5);
+        for (int i = 0; i < size; ++i) {
+            int bucketNum = (oomages.get(i).hashCode() & 0x7FFFFFFF) % M;
+            arr[bucketNum]++;
+        }
+        for (int i = 0; i < M; ++i) {
+            if (arr[i] > up || arr[i] < low) {
+                return false;
+            }
+        }
+        return true;
     }
 }
